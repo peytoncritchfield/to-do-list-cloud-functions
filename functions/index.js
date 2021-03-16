10a6d7f3db8d7d800df91b017147c471aae39098
peyton critchfield <peyton.developer@gmail.com>
2021-03-16 15:26:56 -0400

    fleshed out service endpoints

--- functions/index.js
+++ functions/index.js
@@ -12,25 +12,63 @@ const app = express();
 
 app.use(cors({ origin: true }));
 
-app.post('/user', async (req, res) => {
+app.post('/task', async (req, res) => {
   try {
-      let user = req.body;
-      await admin.firestore().collection("items").add(user);
-      return res.status(200).send('User Added');
+      let task = req.body;
+
+      await admin.firestore().collection("items").add(task)
+      .then((addedTask) => {
+        admin.firestore().collection("items").doc(addedTask.id).update({
+          id: addedTask.id
+        })
+      });
+
+      return res.status(200).send('Task Added');
   } catch (error) {
       return res.status(500).send(error);
   }
 });
 
-app.get('/users', async (req, res) => {
+app.get('/tasks', async (req, res) => {
   try {
-      let users = await admin.firestore().collection("items").get();
-      let snapshot = users.docs.map(doc => doc.data());
+      let tasks = await admin.firestore().collection("items").get();
+      let snapshot = tasks.docs.map(doc => doc.data());
       return res.status(200).send(snapshot);
   } catch (error) {
       return res.status(500).send(error);
   }
-  
+});
+
+app.get('/task', async (req, res) => {
+  try {
+      let id = req.query.id;
+      let task = await admin.firestore().collection("items").doc(id).get();
+      return res.status(200).send(task.data());
+  } catch (error) {
+      return res.status(500).send(error);
+  }
+});
+
+app.delete('/task', async (req, res) => {
+  try {
+      let id = req.query.id;
+      let task = await admin.firestore().collection("items").doc(id).delete();
+      return res.status(200).send(task.data());
+  } catch (error) {
+      return res.status(500).send(error);
+  }
+});
+
+app.patch('/task', async (req, res) => {
+  try {
+      let id = req.query.id;
+      let updatedTask = req.body;
+
+      let task = await admin.firestore().collection("items").doc(id).update(updatedTask);
+      return res.status(200).send('success');
+  } catch (error) {
+      return res.status(500).send(error);
+  }
 });
 
 exports.api = functions.https.onRequest(app);
